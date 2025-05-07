c63fd0ad2eb3b96352d205a07534de3123a5ce46
AlanB <teamlemons.cr@gmail.com>
2025-05-07 17:31:20 -0300

    refactor: update motor control pin definitions and improve movement logic

--- src/VSSS_code.cpp
+++ src/VSSS_code.cpp
@@ -21,26 +21,23 @@
 #define STBY 19
 #define LED_PIN 2
 
-/* Encoder optico-incremental A-B com efeito Hall */
-#define ENC_MOTOR_LEFT_A 34  // Pino de encoder optico-incremental do motor esquerdo A
-#define ENC_MOTOR_LEFT_B 35 // Pino de encoder optico-incremental do motor esquerdo B
-#define ENC_MOTOR_RIGHT_A 12// Pino de encoder optico-incremental do motor direito A
-#define ENC_MOTOR_RIGHT_B 13// Pino de encoder optico-incremental do motor direito B
+#define ENC_MOTOR_LEFT_A 34  // Encoder motor izquierdo A
+#define ENC_MOTOR_LEFT_B 35 // Encoder motor izquierdo B
+#define ENC_MOTOR_RIGHT_A 12// Encoder motor derecho A
+#define ENC_MOTOR_RIGHT_B 13// Encoder motor derecho B
 
 #define PWMA_CHANNEL 0
 #define PWMB_CHANNEL 1
 
 int16_t leftWheel = 0;
 int16_t rightWheel = 0;
-int16_t acc = 0;
-int16_t diff = 0;
-
 volatile int leftPulses = 0;
 volatile int rightPulses = 0;
 unsigned long lastTime = 0;
 float leftRPM = 0;
 float rightRPM = 0;
 int PULSES_PER_REV = 60;
+const int DEADZONE = 10;
 
 void IRAM_ATTR countLeftPulses() {
     leftPulses += (digitalRead(ENC_MOTOR_LEFT_A) > digitalRead(ENC_MOTOR_LEFT_B)) ? 1 : -1;
@@ -55,99 +52,46 @@ void calculateRPM() {
     unsigned long timeElapsed = currentTime - lastTime;
 
     if (timeElapsed >= 1000) {
-        leftRPM = ((float)leftPulses / PULSES_PER_REV) * (60.0*1000/timeElapsed);
-        rightRPM = ((float)rightPulses / PULSES_PER_REV) *(60.0*1000/timeElapsed);
+        leftRPM = ((float)leftPulses / PULSES_PER_REV) * (60.0 * 1000 / timeElapsed);
+        rightRPM = ((float)rightPulses / PULSES_PER_REV) * (60.0 * 1000 / timeElapsed);
 
         leftPulses = 0;
         rightPulses = 0;
         lastTime = currentTime;
 
-        Serial.print("Left RPM: ");
-        Serial.println(leftRPM);
-        Serial.print("Right RPM: ");
-        Serial.println(rightRPM);
+        Serial.print("Left RPM: "); Serial.println(leftRPM);
+        Serial.print("Right RPM: "); Serial.println(rightRPM);
     }
 }
+
 void moveEngine(bool forward, int speed, int num_motor)
 {
-    if (num_motor == 1)
-    {
-        if (forward)
-        {
-            digitalWrite(AIN1, HIGH);
-            digitalWrite(AIN2, LOW);
-        }
-        else
-        {
-            digitalWrite(AIN1, LOW);
-            digitalWrite(AIN2, HIGH);
-        }
-        ledcWrite(PWMA_CHANNEL, speed); // Set motor speed
-    }
-    else if (num_motor == 2)
-    {
-        if (forward)
-        {
-            digitalWrite(BIN1, LOW);
-            digitalWrite(BIN2, HIGH);
-        }
-        else
-        {
-            digitalWrite(BIN1, HIGH);
-            digitalWrite(BIN2, LOW);
-        }
-        ledcWrite(PWMB_CHANNEL, speed); // Set motor speed
+    speed = constrain(speed, 0, 255);
+
+    if (num_motor == 1) { // Motor izquierdo
+        digitalWrite(AIN1, !forward);
+        digitalWrite(AIN2, forward);
+        ledcWrite(PWMA_CHANNEL, speed);
+    } 
+    else if (num_motor == 2) { // Motor derecho (invertimos la lógica)
+        digitalWrite(BIN1, forward);   // <- Cambiamos !forward por forward
+        digitalWrite(BIN2, !forward);  // <- Cambiamos forward por !forward
+        ledcWrite(PWMB_CHANNEL, speed);
     }
 }
 
-void dataReceiver()
-{
-    if (Ps3.isConnected())
-    {
+void dataReceiver() {
+    if (Ps3.isConnected()) {
         ControllerData data = getControllerData();
-        acc = data.ly;
-        diff = data.rx;
+        leftWheel = abs(data.ly) > DEADZONE ? data.ly : 0;
+        rightWheel = abs(data.ry) > DEADZONE ? data.ry : 0;
     }
 }
 
-void findDifferentialGear()
-{
-    if (diff < 0)
-    {
-        if (acc != 0)
-        {
-            leftWheel = abs(acc) - (abs(diff) - (0.125 * abs(diff)));
-            rightWheel = abs(acc);
-        }
-        else
-        {
-            leftWheel = rightWheel = 0;
-        }
-    }
-    else if (diff > 0)
-    {
-        if (acc != 0)
-        {
-            rightWheel = abs(acc) - (abs(diff) - (0.125 * abs(diff)));
-            leftWheel = abs(acc);
-        }
-        else
-        {
-            leftWheel = rightWheel = 0;
-        }
-    }
-    else
-    {
-        leftWheel = rightWheel = abs(acc);
-    }
-}
-
-void setup()
-{
+void setup() {
     Serial.begin(115200);
     Serial.println(Ps3.getAddress());
     delay(2000);
-    // Inicializa o Bluetooth
     controllerConfig(MAC);
 
     pinMode(PWMA, OUTPUT);
@@ -163,7 +107,6 @@ void setup()
 
     ledcSetup(PWMA_CHANNEL, 5000, 8);
     ledcAttachPin(PWMA, PWMA_CHANNEL);
-
     ledcSetup(PWMB_CHANNEL, 5000, 8);
     ledcAttachPin(PWMB, PWMB_CHANNEL);
 
@@ -178,26 +121,22 @@ void setup()
     lastTime = millis();
 }
 
-void loop()
-{
+void loop() {
     dataReceiver();
 
-    findDifferentialGear();
-
-    if (acc < 0)
-    {
+    if (leftWheel > 0)
         moveEngine(1, leftWheel * 2, 1);
-        moveEngine(0, rightWheel * 2, 2);
-    }
-    else if (acc > 0)
-    {
-        moveEngine(0, leftWheel * 2, 1);
-        moveEngine(1, rightWheel * 2, 2);
-    }
+    else if (leftWheel < 0)
+        moveEngine(0, abs(leftWheel) * 2, 1);
     else
-    {
         moveEngine(0, 0, 1);
+
+    if (rightWheel > 0)
+        moveEngine(1, rightWheel * 2, 2);
+    else if (rightWheel < 0)
+        moveEngine(0, abs(rightWheel) * 2, 2);
+    else
         moveEngine(0, 0, 2);
-    }
+
     calculateRPM();
 }
